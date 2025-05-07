9220bd2607b04886327d61dfd3d2b4c14a868cce
vhlebnikov <v.khlebnikov@g.nsu.ru>
2025-05-08 01:22:28 +0700

    update db scheme

--- migrations/1-init.sql
+++ migrations/1-init.sql
@@ -1,8 +1,15 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS news_bows
 (
-    date timestamp not null unique,
-    bow  jsonb     not null
+    url  varchar primary key,
+    bow  jsonb     not null,
+    date timestamp not null unique -- format: yyyy:mm:dd:hh
+
+);
+
+CREATE TABLE IF NOT EXISTS news_done
+(
+    date date not null unique -- format: yyyy-mm-dd
 );
 
 CREATE TABLE IF NOT EXISTS instruments
@@ -20,14 +27,36 @@ CREATE TABLE IF NOT EXISTS stocks
 
 CREATE TABLE IF NOT EXISTS sttm_indexes
 (
+    id            serial primary key,
     index         numeric   not null,
     instrument_id varchar   not null,
     from_time     timestamp not null,
-    to_time       timestamp not null
+    to_time       timestamp not null,
+    alpha         numeric,
+    p_value       numeric,
+    threshold     numeric
+);
+
+CREATE TABLE IF NOT EXISTS sttm_interpretability
+(
+    topics        jsonb   not null, -- {"topic_0": 0, "topic_1": 1}
+    tts           jsonb   not null, -- [[1,2,3], [4,5,6], ...] each internal element represents N topic probability over time
+    sttm_index_id integer not null,
+    foreign key (sttm_index_id) references sttm_indexes (id) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS trading_journal
+(
+    ts   timestamp not null,
+    op   varchar   not null,
+    data jsonb
 );
 
 -- +migrate Down
+DROP TABLE IF EXISTS trading_journal;
+DROP TABLE IF EXISTS sttm_interpretability;
 DROP TABLE IF EXISTS sttm_indexes;
 DROP TABLE IF EXISTS stocks;
 DROP TABLE IF EXISTS instruments;
+DROP TABLE IF EXISTS news_done;
 DROP TABLE IF EXISTS news_bows;
